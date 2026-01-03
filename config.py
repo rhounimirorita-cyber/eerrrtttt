e748acea60bf18a9e2ce8087905c2263374c7349
rhounimirorita-cyber <rhounimirorita@gmail.com>
2026-01-03 22:57:32 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+# ضع التوكن الخاص بك هنا
+TOKEN = "7953606786:AAG5On_R3WztxlbRU0-U1QxdKnr2Pg1ZW54"
+
+# رمز الأدمن
+ADMIN_CODE = "123123NNK"
+
+# التوقيع أسفل الرسائل
+SIGNATURE = "\n\n© @FAKHERDDIN5"
+
+# ملفات البيانات
+DATA_FILE = "bot_data.json"
+PRICES_FILE = "prices.json"
+FILES_DIR = "files"
+
+# رابط قناة البائعين
+SELLER_CHANNEL_URL = "https://t.me/stonexff"
+
+# المنتجات المخصصة لـ Free Fire (جديدة)
+FF_PRODUCTS = {
+    "FLUORITE": "Fluorite",
+    "DRIP": "Drip Client",
+    "BR_MODS": "BR Mods",
+    "HG_CHEATS": "HG Cheats"
+}
+
+# الأزمنة الخاصة بـ HG Cheats
+HG_CHEATS_DURATIONS = ["10", "30"]
+# الأزمنة الخاصة بـ Drip و BR
+COMMON_ANDROID_DURATIONS = ["1", "7", "15", "31"]
+# الأزمنة الخاصة بـ Fluorite (iOS)
+IOS_DURATIONS = ["1", "7", "15", "31"]
